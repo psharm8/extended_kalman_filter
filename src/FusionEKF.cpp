04f96fd0e3ec4da17e9d00aaf96a80bf2293604d
vtretyakov <vtretyakov@synapticon.com>
2018-01-01 19:36:53 +0100

    FusionEKF: use Jacobian matrix in update step for better readability

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -178,7 +178,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
     
