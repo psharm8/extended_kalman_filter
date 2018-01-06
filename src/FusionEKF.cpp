3f4d4f29296717f48a6bcbe2ee8deda0241c5631
vtretyakov <vtretyakov@synapticon.com>
2018-01-06 17:32:02 +0100

    fix code formatting

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,25 +36,25 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
-    //laser measurement matrix
-    H_laser_ << 1, 0, 0, 0,
-                0, 1, 0, 0;
+  //laser measurement matrix
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
     
-    //Jacobian matrix
-    Hj_ <<  1, 1, 0, 0,
-            1, 1, 0, 0,
-            1, 1, 1, 1;
+  //Jacobian matrix
+  Hj_ <<  1, 1, 0, 0,
+          1, 1, 0, 0,
+          1, 1, 1, 1;
 
     //the initial transition matrix F_
-    ekf_.F_ = MatrixXd(4, 4);
-    ekf_.F_ << 1, 0, 1, 0,
-               0, 1, 0, 1,
-               0, 0, 1, 0,
-               0, 0, 0, 1;
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
     
-    //state covariance matrix P
-    ekf_.P_ = MatrixXd(4, 4);
-    ekf_.P_ << 1, 0, 0, 0,
+  //state covariance matrix P
+  ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0, 0, 0,
                0, 1, 0, 0,
                0, 0, 1000, 0,
                0, 0, 0, 1000;
